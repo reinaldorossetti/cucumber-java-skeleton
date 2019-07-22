65925f96f22610ce22f51d974bfc68f661fddcee
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2019-07-22 00:18:00 -0300

    Add files via upload

--- src/test/java/io/cucumber/skeleton/Stepdefs.java
+++ src/test/java/io/cucumber/skeleton/Stepdefs.java
@@ -6,15 +6,24 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 
 public class Stepdefs {
 
     @Before
-    public void doSomethingBefore(Scenario scenario) {
+    public void doSomethingBefore(Scenario scenario) throws Exception {
         String CT = scenario.getSourceTagNames().iterator().next();
         System.out.println("TEST CN: " + CT);
         String cwd = System.getProperty("user.dir");
         System.out.println("Current working directory : " + cwd);
+        String csv_path = (cwd + "\\data_mass\\data_mass_scenario.csv");
+        ReaderCSV csv_file = new ReaderCSV();
+        ArrayList<String> result = csv_file.oneByOne(csv_path, CT);
+        result.forEach(System.out::println);
     }
 
     @Given("^I have (\\d+) cukes in my belly$")
@@ -26,7 +35,7 @@ public class Stepdefs {
     @When("I wait {int} hour")
     public void i_wait_hour(Integer int1) {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println(int1);
+        System.out.println("\n" + int1);
     }
 
     @Then("my belly should {string}")
